8aeea5df8d77ccc5fa26f8bdf978c481d63603d9
Ammar Massoud <76789647+ammar-Mohamed-elz3eeem@users.noreply.github.com>
2024-12-22 12:08:40 -0500

    Add serializers, viewsets and urls for all models

--- alx_travel_app/listing/urls.py
+++ alx_travel_app/listing/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
-from .views import ListingViewSet
+from .views import (
+    ListingViewSet,
+    BookingViewSet,
+    ReviewViewSet
+)
 
 router = routers.DefaultRouter()
-router.register('listings', ListingViewSet, basename='listing'),
+router.register(r'listings/', ListingViewSet, basename='listing'),
+router.register(r'booking/', BookingViewSet, basename='booking'),
+router.register(r'reviews/', ReviewViewSet, basename='review'),
 
 urlpatterns = [
     path('', include(router.urls))
--- alx_travel_app/listing/views.py
+++ alx_travel_app/listing/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from rest_framework.viewsets import generics, GenericViewSet
 from rest_framework import permissions
-from .serializers import ListingSerializer
-from .models import Listing
+from .serializers import (
+    ListingSerializer,
+    BookingSerializer,
+    ReviewSerializer
+)
+from .models import Listing, Booking, Review
 
 
-class ListingViewSet(GenericViewSet, generics.ListCreateAPIView):
+class ListingViewSet(GenericViewSet):
     queryset = Listing.objects.all()
     serializer_class = ListingSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class BookingViewSet(GenericViewSet):
+    queryset = Booking.objects.all()
+    serializer_class = BookingSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class ReviewViewSet(GenericViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
     permission_classes = (permissions.AllowAny,)
