4414b3d0bef522d2b5b51d1180266932da42523e
Ammar Massoud <ammarmohamedthez@gmail.com>
2024-12-23 04:11:35 -0500

    Fix pycodestyle in serializers

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Listing, Booking, Review
 from django.contrib.auth.models import User
 
+
 class BookingSerializer(serializers.HyperlinkedModelSerializer):
     booking_id = serializers.HyperlinkedIdentityField(view_name='booking-detail')
 
     class Meta:
         model = Booking
-        fields = ('url', 'booking_id', 'listing_id', 'user_id', 'start_date', 'end_date', 'total_price')
+        fields = ('url', 'booking_id', 'listing_id', 'user_id',
+                  'start_date', 'end_date', 'total_price')
 
 
 class ReviewSerializer(serializers.HyperlinkedModelSerializer):
@@ -25,8 +27,10 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class ListingSerializer(serializers.HyperlinkedModelSerializer):
-    host_id = serializers.HyperlinkedRelatedField(view_name='user-detail', queryset=User.objects.all())
+    host_id = serializers.HyperlinkedRelatedField(view_name='user-detail',
+                                                  queryset=User.objects.all())
 
     class Meta:
         model = Listing
-        fields = ('name', 'host_id', 'description', 'location', 'pricepernight', 'listing_reviews', 'listing_bookings')
+        fields = ('name', 'host_id', 'description', 'location',
+                  'pricepernight', 'listing_reviews', 'listing_bookings')
